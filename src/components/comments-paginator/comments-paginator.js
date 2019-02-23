f717e45b831f1d26b26a2a07a591723510c122d9
Alex Fedotov <alfumit@gmail.com>
2019-02-23 22:44:52 +0300

    Finalizing HW6

--- src/components/comments-paginator/comments-paginator.js
+++ src/components/comments-paginator/comments-paginator.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import Comment from '../comment/comment'
 import { loadPageComments } from '../../ac'
 import { totalCommentsSelector, commentsPageLoadingSelector, commentsPageIdsSelector } from '../../selectors'
+import { COMMENTS_PER_PAGE } from '../../constants'
 
 class CommentsPaginator extends Component {
   componentDidMount() {
@@ -10,9 +12,20 @@ class CommentsPaginator extends Component {
   }
   
   render() {
+    const pages = [...Array(Math.ceil(this.props.total / COMMENTS_PER_PAGE)).keys()];
     return (
       <div>
-        <h1>Ay Paginator {this.props.page}</h1>
+        <h1>Paginator page {this.props.page}</h1>
+        <ul>
+          { pages.map(num => {
+            const pageNum = num ++;
+            return (
+            <li key={pageNum} style={{listStyleType: 'none', display: 'inline-block', margin: '0 5px'}}>
+              <a href={`/comments/${pageNum}`}>
+                {pageNum}
+              </a>
+            </li>)} )}
+        </ul>
         {this.props.loaded &&
         <ul>
           {this.props.comments.map((id) => {
